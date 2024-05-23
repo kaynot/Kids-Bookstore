878ce8eb20ca91cd36dfd2c5c1223a7cf1f628db
kaynot <percykaynot@gmail.com>
2024-05-23 15:03:52 +0000

    loginModal

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import './App.css';
 import { LoginPage } from './Components/Pages/loginPage';
-import { Modal } from './Templates/modal';
 import { Navbar } from './Components/Pages/navbar';
 import { TopLayer } from './Components/Pages/topLayer';
 import { BookPage } from './Components/Pages/bookPage';
 import BookPage2 from './Components/Pages/bookPage2';
 import Footer from './Components/Pages/footer';
+
 import { SearchProvider } from './Components/Pages/searchProvider';
+import { LoginModal } from './Templates/loginModal';
 
 
 
@@ -19,6 +20,7 @@ function App() {
    <BookPage/>
    <BookPage2/>
    <Footer/>
+   {/* <LoginModal/> */}
     </div>
   );
 }
